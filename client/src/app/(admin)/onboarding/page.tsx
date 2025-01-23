59c149bd0c71040813059102bce118606d8c11c3
Arielcito <serato.arieli@gmail.com>
2025-01-23 12:55:24 -0300

    remove client

--- client/src/app/(admin)/onboarding/page.tsx
+++
@@ -1,9 +0,0 @@
-import { OnboardingPageComponent } from "@/components/Onboarding/Onboarding";
-
-export default function OnboardingPage() {
-  return (
-    <div className="min-h-screen bg-white dark:bg-black">
-      <OnboardingPageComponent     />
-    </div>
-  );
-} 
--- client/src/types/reservation.ts
+++
@@ -1,6 +0,0 @@
-export type RESERVATION = {
-  date: string;
-  time: string;
-  field: string;
-  client: string;
-};
