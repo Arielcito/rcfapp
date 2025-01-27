83b117bcf4d736a77f358a726b7458419e1198bd
Arielcito <serato.arieli@gmail.com>
2025-01-26 23:32:20 -0300

    remove app

--- rcf_app/lib/bindings/booking_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import '../controllers/booking/booking_controller.dart';
-
-class BookingBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<BookingController>(() => BookingController());
-  }
-} 
