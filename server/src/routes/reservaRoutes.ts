5b39aca8703b5436967b93eca9ea250424141bbc
Arielcito <serato.arieli@gmail.com>
2025-03-14 14:29:46 -0300

    feat: get all reservas

--- server/src/routes/reservaRoutes.ts
+++ server/src/routes/reservaRoutes.ts
@@ -6,18 +6,18 @@ const router = Router();
 const reservaController = new ReservaController();
 
 // Rutas GET
-router.get('/', authenticateToken, reservaController.getReservas);
-router.get('/user/bookings', authenticateToken, reservaController.getUserBookings);
-router.get('/owner/:id', authenticateToken, reservaController.getReservasByOwner);
-router.get('/owner/:date/:ownerId', authenticateToken, reservaController.getReservasByDate);
-router.get('/:id', authenticateToken, reservaController.getReservaById);
+router.get('/', reservaController.getReservas);
+router.get('/user/bookings', reservaController.getUserBookings);
+router.get('/owner/:id', reservaController.getReservasByOwner);
+router.get('/owner/:date/:ownerId', reservaController.getReservasByDate);
+router.get('/:id', reservaController.getReservaById);
 
 // Rutas POST
-router.post('/', authenticateToken, reservaController.createReserva);
-router.post('/check', authenticateToken, reservaController.checkReservaAvailability);
-router.post('/available-times', authenticateToken, reservaController.getAvailableTimes);
+router.post('/', reservaController.createReserva);
+router.post('/check', reservaController.checkReservaAvailability);
+router.post('/available-times', reservaController.getAvailableTimes);
 
 // Rutas PUT
-router.put('/:id', authenticateToken, reservaController.updateReserva);
+router.put('/:id', reservaController.updateReserva);
 
 export default router; 
